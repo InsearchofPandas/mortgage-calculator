7425caa6cbd9a44b218ed19fd2e23eacb8b6da69
InsearchofPandas <kylerhumphrey@gmail.com>
2019-02-28 09:31:34 -0800

    complete responsive design

--- src/App.js
+++ src/App.js
@@ -8,198 +8,192 @@ class App extends Component {
     super(props);
     this.state = {
       loanAmount: '',
-      loanAmountDisplay:'',
+      loanAmountDisplay: '',
       loanRate: 3.0,
       loanYears: 30,
       statusMessage: "Enter the loan amount"
     }
   }
-handleChange = event => {
-  switch (event.target.name) {
-    case "loanAmount":
-      let statusMessage
-      let loanAmountDisplay
-      let loanAmount = event.target.value
-      loanAmount = event.target.value.replace(/ /g, 'k')
+  handleChange = event => {
+    switch (event.target.name) {
+      case "loanAmount":
+        let statusMessage
+        let loanAmountDisplay
+        let loanAmount = event.target.value
+        loanAmount = event.target.value.replace(/ /g, 'k')
         loanAmount = loanAmount.replace(/,/g, '')
-        console.log(loanAmount)
-      if (isNaN(loanAmount) === true) {
-        statusMessage = "Only Numbers Accepted"
-        loanAmountDisplay = this.state.loanAmountDisplay
-       } else if (event.target.value.length > 11) {
-         loanAmountDisplay = this.state.loanAmountDisplay
-         statusMessage = "Max Input Amount"
-       }  else if (loanAmount < 10000) {
+        if (isNaN(loanAmount) === true) {
+          statusMessage = "Only Numbers Accepted"
+          loanAmountDisplay = this.state.loanAmountDisplay
+        } else if (event.target.value.length > 11) {
+          loanAmountDisplay = this.state.loanAmountDisplay
+          statusMessage = "Max Input Amount"
+        } else if (loanAmount < 10000) {
           loanAmountDisplay = loanAmount
           statusMessage = "Minimum Loan Amount 10,000"
-        }  else if (loanAmount >= 10000) {
+        } else if (loanAmount >= 10000) {
           loanAmountDisplay = loanAmount
           statusMessage = ""
         }
 
-        this.setState({
-          loanAmount: loanAmount,
-          loanAmountDisplay: loanAmountDisplay,
-           statusMessage: statusMessage
-         })
+        this.setState({loanAmount: loanAmount, loanAmountDisplay: loanAmountDisplay, statusMessage: statusMessage})
 
-      break;
-    case "loanRate":
-      this.setState({loanRate: event.target.value})
-      break;
-    case "loanYears":
-      this.setState({loanYears: event.target.value})
-      break
-    default:
+        break;
+      case "loanRate":
+        this.setState({loanRate: event.target.value})
+        break;
+      case "loanYears":
+        this.setState({loanYears: event.target.value})
+        break
+      default:
 
+    }
   }
-}
 
   render() {
-// update loan amount to display with commas
+    // update loan amount to display with commas
     let outLoanAmountDisplay = this.state.loanAmountDisplay
-          outLoanAmountDisplay = Math.floor(outLoanAmountDisplay)
+      outLoanAmountDisplay = Math.floor(outLoanAmountDisplay)
       outLoanAmountDisplay = outLoanAmountDisplay.toLocaleString(undefined, {
-          minimumFractionDigits: 0,
-          maximumFractionDigits: 0
-          })
-       console.log()
-
-// loan  calculations
-    let monthlyPayment,
-      totalPayment,
-      displayMonthlyPayment,
-      displayTotalPayment
-    const p = this.state.loanAmount
-    const r = (this.state.loanRate * .01) / 12
-    const n = this.state.loanYears * 12
-    if (p < 1000) {
-      displayMonthlyPayment = ""
-      displayTotalPayment = ""
-    } else {
-      monthlyPayment = p * (((r * (1 + r) ** n)) / (((1 + r) ** n) - 1))
-      totalPayment = monthlyPayment * n
-      displayMonthlyPayment = monthlyPayment.toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
       })
-      displayTotalPayment = totalPayment.toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      })
-    }     if (isNaN(monthlyPayment)) {
-          displayMonthlyPayment = ""
-        }
-        if (isNaN(totalPayment)) {
-          displayTotalPayment = ""
-        }
 
+      // loan  calculations
+      let monthlyPayment,
+        totalPayment,
+        displayMonthlyPayment,
+        displayTotalPayment
+      const p = this.state.loanAmount
+      const r = (this.state.loanRate * .01) / 12
+      const n = this.state.loanYears * 12
+      if (p < 1000) {
+        displayMonthlyPayment = ""
+        displayTotalPayment = ""
+      } else {
+        monthlyPayment = p * (((r * (1 + r) ** n)) / (((1 + r) ** n) - 1))
+        totalPayment = monthlyPayment * n
+        displayMonthlyPayment = monthlyPayment.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        })
+        displayTotalPayment = totalPayment.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        })
+      }
+      if (isNaN(monthlyPayment)) {
+        displayMonthlyPayment = ""
+      }
+      if (isNaN(totalPayment)) {
+        displayTotalPayment = ""
+      }
 
-    return (<div className="bg-blue-lightest h-screen">
+      return (<div className="bg-blue-lightest h-screen">
 
-      <header className="flex">
-        <h1 className="w-full bg-blue-lighter text-center my-6 py-4 font-normal ">Mortgage Payment Calculator</h1>
-      </header>
+        <header className="flex">
+          <h1 className="w-full bg-blue-lighter text-center my-6 py-4 font-normal ">Mortgage Payment Calculator</h1>
+        </header>
 
-      <div className="flex justify-center mb-3 h-4">
-        <div className="justify-center">{this.state.statusMessage}</div>
-      </div>
-
-      <div className="flex justify-center text-3xl">
-        <div className="flex justify-center w-2/3">
-          <div className="w-3/5 text-center bg-grey-dark p-3 m-1">
-            Loan Amount
-          </div>
-          <TextBox name="loanAmount" value={outLoanAmountDisplay}  onChange={this.handleChange}/>
+        <div className="flex justify-center mb-3 h-4">
+          <div className="justify-center">{this.state.statusMessage}</div>
         </div>
-      </div>
 
-      <div className="flex justify-center text-3xl">
-        <div className="flex justify-center w-2/3">
-          <div className="w-3/5 text-center bg-grey-dark p-3 m-1">
-            Interest Ratet
+        <div className="lg:flex justify-center text-3xl">
+          <div className="lg:flex justify-center lg:w-2/3">
+            <div className="w-full lg:w-1/2  text-center bg-grey-dark p-3 m-1">
+              Loan Amount
+            </div>
+            <TextBox name="loanAmount" value={outLoanAmountDisplay} onChange={this.handleChange}/>
           </div>
-          <SelectBox name='loanRate' value={this.state.loanRate} onChange={this.handleChange} options={[
-              "3.00",
-              "3.25",
-              "3.50",
-              "3.75",
-              "4.00",
-              "4.25",
-              "4.50",
-              "4.75",
-              "5.00",
-              "5.25",
-              "5.50",
-              "5.75",
-              "6.00",
-              "6.25",
-              "6.50",
-              "6.75"
-            ]}/>
         </div>
-      </div>
 
-      <div className="flex justify-center text-3xl">
-        <div className="flex justify-center w-2/3">
-          <div className="w-3/5 text-center bg-grey-dark p-3 m-1">
-            Loan Years
+        <div className="lg:flex justify-center text-3xl">
+          <div className="lg:flex justify-center lg:w-2/3">
+            <div className="w-full lg:w-1/2 text-center bg-grey-dark p-3 m-1">
+              Interest Ratet
+            </div>
+            <SelectBox name='loanRate' value={this.state.loanRate} onChange={this.handleChange} options={[
+                "3.00",
+                "3.25",
+                "3.50",
+                "3.75",
+                "4.00",
+                "4.25",
+                "4.50",
+                "4.75",
+                "5.00",
+                "5.25",
+                "5.50",
+                "5.75",
+                "6.00",
+                "6.25",
+                "6.50",
+                "6.75"
+              ]}/>
           </div>
-          <SelectBox name="loanYears" value={this.state.loanYears} onChange={this.handleChange} options={[
-              30,
-              29,
-              28,
-              27,
-              26,
-              25,
-              24,
-              23,
-              22,
-              21,
-              20,
-              19,
-              18,
-              17,
-              16,
-              15,
-              14,
-              13,
-              12,
-              11,
-              10,
-              9,
-              8,
-              7,
-              6,
-              5,
-              4,
-              3,
-              2,
-              1
-            ]}/>
         </div>
-      </div>
 
-      <div className="flex justify-center text-3xl">
-        <div className="flex justify-center w-2/3">
-          <div className="w-3/5 text-center bg-grey-dark p-3 m-1">
-            Monthly Payment
+        <div className="lg:flex justify-center text-3xl">
+          <div className="lg:flex justify-center lg:w-2/3">
+            <div className="w-full lg:w-1/2 text-center bg-grey-dark p-3 m-1">
+              Loan Years
+            </div>
+            <SelectBox name="loanYears" value={this.state.loanYears} onChange={this.handleChange} options={[
+                30,
+                29,
+                28,
+                27,
+                26,
+                25,
+                24,
+                23,
+                22,
+                21,
+                20,
+                19,
+                18,
+                17,
+                16,
+                15,
+                14,
+                13,
+                12,
+                11,
+                10,
+                9,
+                8,
+                7,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1
+              ]}/>
           </div>
-          <div className="w-3/5 text-center bg-grey-light p-3 m-1">{displayMonthlyPayment}</div>
         </div>
-      </div>
 
-      <div className="flex justify-center text-3xl">
-        <div className="flex justify-center w-2/3">
-          <div className="w-3/5 text-center bg-grey-dark p-3 m-1">
-            Total Amount Paid
+        <div className="lg:flex justify-center text-3xl">
+          <div className="lg:flex justify-center lg:w-2/3">
+            <div className="w-full lg:w-1/2 text-center bg-grey-dark p-3 m-1">
+              Monthly Payment
+            </div>
+            <div className="w-full lg:w-1/2 text-center bg-grey-light p-3 m-1">{displayMonthlyPayment}</div>
           </div>
-          <div className="w-3/5 text-center bg-grey-light p-3 m-1">{displayTotalPayment}</div>
         </div>
-      </div>
 
-    </div>)
+        <div className="lg:flex justify-center text-3xl">
+          <div className="lg:flex justify-center lg:w-2/3">
+            <div className="w-full lg:w-1/2 text-center bg-grey-dark p-3 m-1">
+              Total Amount Paid
+            </div>
+            <div className="w-full lg:w-1/2 text-center bg-grey-light p-3 m-1">{displayTotalPayment}</div>
+          </div>
+        </div>
+
+      </div>)
+    }
   }
-}
 
-export default App;
+  export default App;
--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -2,16 +2,7 @@ import React from 'react'
 
 class TextBox extends React.Component {
   render() {
-    return(
-      <input
-        type="text"
-        className="w-3/5 text-center bg-grey-light p-3 m-1 text"
-        name={this.props.name}
-        value={this.props.value}
-        placeholder="Enter Here"
-        onChange={this.props.onChange}
-      />
-    )
+    return (<input type="text" className="w-full lg:w-1/2 text-center bg-grey-light p-3 m-1" name={this.props.name} value={this.props.value} placeholder="Enter Here" onChange={this.props.onChange}/>)
   }
 }
 
